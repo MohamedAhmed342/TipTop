2a794ebc30467cc76c15d0e4458d7201e590556a
salahsaed5 <salahsaed599@gmail.com>
2023-12-08 17:34:07 +0200

    modify on home page and auth

--- Front-End/src/components/auth/SignUp.jsx
+++ Front-End/src/components/auth/SignUp.jsx
@@ -73,7 +73,7 @@ const SignUp = () => {
   };
 
   return (
-    
+    <>
     <div className="bodyy">
     <div className="wrapper">
       <form action="" onSubmit={handleSubmit}>
@@ -111,6 +111,76 @@ const SignUp = () => {
       </form>
     </div>
     </div>
+    
+    <footer>
+    <div class="footer_main">
+      <div class="tag">
+        <h1>Contact</h1>
+        <a href="#">
+          <i class="fa-solid fa-house"></i>123/Colombo/Sri Lanka
+        </a>
+        <a href="#">
+          <i class="fa-solid fa-phone"></i>+94 12 345 6789
+        </a>
+        <a href="#">
+          <i class="fa-solid fa-envelope"></i>contact@gmail.com
+        </a>
+      </div>
+
+      <div class="tag">
+        <h1>Get Help</h1>
+        <a href="#" class="center">
+          FAQ
+        </a>
+        <a href="#" class="center">
+          Shipping
+        </a>
+        <a href="#" class="center">
+          Returns
+        </a>
+        <a href="#" class="center">
+          Payment Options
+        </a>
+      </div>
+
+      <div class="tag">
+        <h1>Our Stores</h1>
+        <a class="center">
+          Sri Lanka
+        </a>
+        <a class="center">
+          USA
+        </a>
+        <a  class="center">
+          India
+        </a>
+        <a  class="center">
+          Japan
+        </a>
+      </div>
+
+      <div class="tag">
+        <h1>Follow Us</h1>
+        <div class="social_link">
+          <a href="#">
+            <i class="fa-brands fa-facebook-f"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-linkedin-in"></i>
+          </a>
+        </div>
+      </div>
+
+      
+    </div>
+  </footer>
+    </>
   );
 };
 
